7e39d3a4b8d2a07e8db90381ce1c51fa8991a330
[Angus L'Herrou] <[piraka@brandeis.edu]>
2019-11-09 13:29:25 -0500

    first vscode commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cstel_mva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Max maxwell = new Max();
+            Console.WriteLine(maxwell.getName());
+        }
+    }
+
+    class Max
+    {
+        private string name;
+        public Max()
+        {
+            name = "Max";
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
